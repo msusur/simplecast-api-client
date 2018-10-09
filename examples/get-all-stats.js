5782fb078cf6ec2cd1eb7093bd5d78454be15b0f
Mert Susur <mail@mertsusur.com>
2018-10-09 17:00:40 +0100

    Adds example of usage

--- examples/get-all-stats.js
+++ examples/get-all-stats.js
@@ -1,6 +1,9 @@
 const SimpleCastClient = require('../src/simplecast-client');
 
+// Usage: node examples/get-all-stats.js API_KEY
+
 const client = new SimpleCastClient({ apikey: process.argv[2] });
+let podcastId;
 client.podcasts
   .getPodcasts()
   .then(podcasts => {
@@ -8,7 +11,16 @@ client.podcasts
       `# of Podcasts: ${podcasts.length}.
       Getting the episodes of ${podcasts[0].title}`
     );
-    return client.episodes.getEpisodes(podcasts[0].id);
+    podcastId = podcasts[0].id;
+    return client.episodes.getEpisodes(podcastId);
+  })
+  .then(episodes => {
+    console.log(`# of episodes: ${episodes.length}
+    Getting the stats for episode ${episodes[13].title}`);
+    return client.statistics.getEpisodeStats(podcastId, episodes[13].id, {});
+  })
+  .then(stats => {
+    console.log(`Total listens: ${stats.total_listens}`);
   })
   .catch(error => {
     console.log({ response: error.message });
